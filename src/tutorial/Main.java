236a89a59ba9f32d71545fbf12616cf037f9ea80
miguelabautista <filan3001@gmail.com>
2017-08-01 10:40:57 +0200

    first commit

---
+++ src/tutorial/Main.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+/**
+ * @version 0.1
+ * @Author Miguel Bautista
+ * @Date 8/1/2017
+ * @since 0.1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
